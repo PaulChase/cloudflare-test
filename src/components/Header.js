4e0b9247d160d377b059bec17274fd92958ead1b
Ajonye Paul <ajonyechase@gmail.com>
2025-06-07 01:58:16 +0100

    adjust nav links

--- src/components/Header.js
+++ src/components/Header.js
@@ -8,15 +8,12 @@ export default function Header() {
 	return (
 		<div className=" p-3 lg:p-4 flex justify-between items-center max-w-7xl mx-auto">
 			{/* <Image src="/logo.png" alt="logo" width={70} height={70} /> */}
-			<h1>{process.env.NEXT_PUBLIC_API_NAME || "My Website"}</h1>
+			<Link href={"/"} className=" block p-4 ">
+				<h1>{process.env.NEXT_PUBLIC_API_NAME || "My Website"}</h1>
+			</Link>
 
 			<div className=" hidden lg:block">
 				<ul className=" font-medium text-gray-700 flex items-center gap-4">
-					<li>
-						<Link href={"/"} className=" block p-4 ">
-							Home
-						</Link>
-					</li>
 					<li>
 						<Link href={"/about-us"} className="  block p-4">
 							About Us
