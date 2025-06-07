cef35ae6e37c596879d551091bcd8ca18f4152d9
Ajonye Paul <ajonyechase@gmail.com>
2025-06-07 01:09:34 +0100

    setup cloud flare

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,14 +3,10 @@ import "./globals.css";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
 
-const inter = Inter({
-	subsets: ["latin"],
-});
-
 export default function RootLayout({ children }) {
 	return (
 		<html lang="en">
-			<body className={`${inter.className}  antialiased`}>
+			<body className={` antialiased`}>
 				<Header />
 				{children}
 				<Footer />
